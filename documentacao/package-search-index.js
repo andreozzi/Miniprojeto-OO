44f0654b0945355134547358340200237aee97c0
Victor Andreozzi <victor.andreozzi.dev@outlook.com>
2023-07-02 19:22:31 -0300

    Documentação atualizada

--- documentacao/package-search-index.js
+++ documentacao/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"control"},{"l":"model"},{"l":"view"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"control"},{"l":"model"},{"l":"testes"},{"l":"view"}];updateSearchResults();
